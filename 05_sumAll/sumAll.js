128aa8491016da60ba4562abf2be50d096f1fb26
mased2002 <edsam2302@gmail.com>
2023-07-16 23:24:52 +0200

    this is the finished exercise that checks all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if(int1 < 0 || int2 < 0 && typeof int1 != "number"  || typeof int2 != "number"){
+        return "ERROR"
+    }else if (int1 < int2) {
+        let newnum = 0;
+        for(let i = int1; i <= int2; i++){
+            newnum+=i 
+        }
+        return newnum
+    }else if(int1 > int2){
+        let newnum = 0;
+        for(let i = int1; i>= int2; i--){
+            newnum+=i;
+        }
+        return newnum
+    }
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
