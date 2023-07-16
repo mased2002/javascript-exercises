97632ba05a1f227626d58ba8594e243d89ceaefd
mased2002 <edsam2302@gmail.com>
2023-07-16 18:00:15 +0200

    reversing string works perfectly with all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const newstr = string.split('')
+    let result = [];
+    for (let i = newstr.length -1; i >= 0; i--){
+        result.push(newstr[i])
+    }
+    return result.join("")
 };
 
 // Do not edit below this line
