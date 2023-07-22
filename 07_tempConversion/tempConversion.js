948ddc83a4ee45e30a466f0103451c186e7bfcd2
mased2002 <edsam2302@gmail.com>
2023-07-22 23:25:56 +0200

    works well and passes all the tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const convertToCelsius = function(farn) {
+const convertToCelsius = function(fahrenheit) {
   let times = 5/9
-  return Math.round(((farn - 32) * times) * 10) /10 
+  return Math.round(((fahrenheit - 32) * times) * 10) /10 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  return Math.round(((celcius * 9/5) + 32) * 10) /10
 };
 
 // Do not edit below this line
