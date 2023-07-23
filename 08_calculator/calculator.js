2623a17bf84c7f7e3b22f436217669cac890c98c
mased2002 <edsam2302@gmail.com>
2023-07-23 20:01:01 +0200

    this is the calculator fully working

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, curval) => {
+		return total += curval
+	}, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+	return args.reduce((total, curval) => {
+    return total *= curval
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  var result = 1
+  for (let i = 0; i < b; i++){
+     result *= a 
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    let result = 0 
+    let factor = a;
+    if(a === 0 || a === 1){
+      result = 1
+    }else {
+	for (let i = a-1; i > 0; i--){
+    factor *= i
+     result = factor; 
+  }
+}
+  return result
 };
 
 // Do not edit below this line
