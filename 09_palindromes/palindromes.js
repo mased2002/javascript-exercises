b54eb3114bac1fe2ba7eb687c8c8e6d34162d841
mased2002 <edsam2302@gmail.com>
2023-07-27 19:38:05 +0200

    works with the first three tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+        
+            var punctRemoved = removepunct(str)
+            const checkPalindromes = []
+            punctRemoved = punctRemoved.split('')
+            for(let i = punctRemoved.length -1; i >= 0; i--){
+                checkPalindromes.push(punctRemoved[i])
+            }
+            console.log(checkPalindromes)
+            console.log(punctRemoved)
+            if(checkPalindromes.join('') === punctRemoved.join('')){
+                return true;
+            }else{
+                return false;
+            }
+            
+            function removepunct(s){
+                const alaphabet = 'abcdefghijklmnopqrstuvwxyz'
+                const alphabetOnly  = []
+            let result = null
+                for (letter of s.toLowerCase()){
+                    if(alaphabet.includes(letter)){
+                     alphabetOnly.push(letter)
+                    }
+                }
+                result = alphabetOnly.join('')
+                return result;
+            }
 };
 
 // Do not edit below this line
