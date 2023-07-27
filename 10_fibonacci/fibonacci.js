7842d2a4bb8700f5bfe643efc479fda365991fd1
mased2002 <edsam2302@gmail.com>
2023-07-27 20:55:06 +0200

    works perfectly well with the tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(idx) {
+    if(idx ==0 ){
+        return 0
+    }
+    if(idx == 1){
+        return 1
+    }
+    if(idx < 0){
+        return "OOPS"
+    }
+let curNum = 1
+let lastNum = 1
+    for (let i = 2; i < idx; i++){
+        let fibo = lastNum + curNum
+        lastNum = curNum
+        curNum = fibo
+        
+    }
+    return curNum
 };
 
 // Do not edit below this line
