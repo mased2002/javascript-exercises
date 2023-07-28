13f8cc31db3e970d0ddd9558ccf22593bf2f27de
mased2002 <edsam2302@gmail.com>
2023-07-28 14:38:40 +0200

    works with all the tests perfectly, was hard though

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const newarr = arr.map((el) => {
+        if(el.yearOfDeath === undefined){
+            el.yearOfDeath = (new Date()).getFullYear();
+        }else if (el.yearOfBirth === undefined){
+            el.yearOfBirth ===(new Date()).getFullYear();
+        }
+        return (el.yearOfDeath - el.yearOfBirth)
+    })
+    console.table(newarr)
+    let result = 0;
+    
+    for(let i = 0; i < newarr.length; i++ ){
+        // console.log("yasssss alors on danse" + newarr[result])
+        // console.log("this is the newarr:" + newarr[i])
+        if (newarr[i] > newarr[result]){
+            // console.log(newarr[i])
+            result = i;
+            // console.log("this is the result :" + result)
+        }
+    }
+    return arr[result];
 };
 
 // Do not edit below this line
